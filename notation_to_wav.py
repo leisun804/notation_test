276a35ea8643a0bfb7e8f542722fa36e3c273818
Leo Sun <kevinleo987@gmail.com>
2024-12-02 13:44:08 +0800

    initial_commit

---
+++ notation_to_wav.py
@@ -0,0 +1,9 @@
+from note_construct import *
+
+file = './notation/note_test8.png'
+des = './wav_output/note_test8.wav'
+
+exprs = notation_to_parameters(file)
+for i in range(len((exprs[0]))):
+    expr = (exprs[0][i], exprs[1][i], exprs[2][i], exprs[3][i], exprs[4][i], exprs[5][i], exprs[6][i])
+    note_construct(expr, des)
